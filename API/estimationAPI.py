8fdf175edd5037a9a96f2e4f269dba5ded0a61fc
Josefine Bexelius <josbex-4@student.ltu.se>
2020-04-27 15:49:59 +0200

    added some CRUD funtionality to the API

--- API/estimationAPI.py
+++ API/estimationAPI.py
@@ -4,18 +4,36 @@ import flask
 from flask import request, jsonify
 import sqlite3
 
-@app.route
+@app.route('/', methods=['POST', 'GET'])
 def index():
     return null
 
 def makePrediction(id, month):
-    return null
+    data = getProductHistory(id)
+    months = data.values()
+    rates = data.keys()
+    predictedRate = API.naiveBayes.makePrediction(API.naiveBayes.trainModel(months, rates), month)
+    return predictedRate
 
 def updateEstimation(id, month):
-    return null
+    month_stmt = "SELECT id FROM " + API.util.db_constants['MONTHS_TABLE'] + " WHERE month=?"
+    month_field = [month]
+    month_data = API.util.getData(month_stmt, month_field)
+    stmt = "UPDATE " + API.util.db_constants['ESTIMATIONS_TABLE'] + " WHERE month_id=? AND product_id=?"
+    fields = [month_data['id'], id]
+    lastrowid = API.util.executeQuery(stmt, fields)
+    return lastrowid
 
 def getProductHistory(id):
-    return null
+    stmt = "SELECT * FROM " + API.util.db_constants['PRODUCT_HISTORY_TABLE'] + "wHERE product_id=?" #TODO: make constants of db columns.
+    field = [id]
+    data = API.util.getData(stmt, field)
+    return data
 
 def sendPrediction():
     return null
+
+#Functions for webpage 
+#--------------------------------------------------------------------
+
+
--- API/util.py
+++ API/util.py
@@ -1,3 +1,5 @@
+import sqlite3
+from sqlite3 import Error
 
 db_constants = {"PRODUCT_TABLE" : "products",
                 "MONTHS_TABLE" : "months", 
@@ -5,4 +7,33 @@ db_constants = {"PRODUCT_TABLE" : "products",
                 "PRODUCT_HISTORY_TABLE" : "history",
                 "ESTIMATIONS_TABLE" : "estimatedRates"
                 }
+
+def getDBConnection(db):
+    try:
+        conn = sqlite3.connect(db)
+    except Error as e:
+        print(e)
+    return conn
+
+
+def dict_factory(cursor, row):
+    d = {}
+    for idx, col in enumerate(cursor.description):
+        d[col[0]] = row[idx]
+    return d
+
+def getData(stmt, fields):
+    conn = getDBConnection('test.db')
+    conn.row_factory = dict_factory
+    cur = conn.cursor()
+    data = cur.execute(stmt, fields).fetchall()
+    return data
+
+def executeQuery(stmt, fields):
+    conn = getDBConnection('test.db')
+    cur = conn.cursor()
+    cur.execute(stmt, fields)
+    conn.commit()
+    return cur.lastrowid
+
 
